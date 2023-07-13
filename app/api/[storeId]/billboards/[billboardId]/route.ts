e0e6fb05aa30e42251b6b601083a29fb1a40827e
EdwardMelendezM <192666@unsaac.edu.pe>
2023-07-13 18:51:42 -0500

    adding cell action for the table as copy, update and delete billboard

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -69,7 +69,7 @@ export async function PATCH(
 
     const billboard = await prismadb.billboard.updateMany({
       where:{
-        id:params.storeId,
+        id: params.billboardId,
       },
       data:{
         label,
