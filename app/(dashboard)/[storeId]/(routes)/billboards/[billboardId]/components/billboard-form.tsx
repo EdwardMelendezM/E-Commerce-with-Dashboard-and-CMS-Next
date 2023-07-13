d3816689c4817c70316c4b5a8af1fa6ba4890a91
EdwardMelendezM <192666@unsaac.edu.pe>
2023-07-13 17:49:26 -0500

    adding data-table and columns for billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -26,8 +26,6 @@ import { Input } from '@/components/ui/input';
 import { toast } from 'react-hot-toast';
 import { useParams, useRouter } from 'next/navigation';
 import AlertModal from '@/components/modals/alert-modal';
-import { ApiAlert } from '@/components/ui/api-alert';
-import { useOrigin } from '@/hooks/use-origin';
 import ImageUpload from '@/components/ui/image-upload';
 
 const formSchema = z.object({
@@ -55,7 +53,6 @@ const BillboardForm:React.FC<BillboardFormProps> = ({
   const params = useParams()
   const router = useRouter()
 
-  const origin = useOrigin()
 
   const title = initialData ? 'Edit billboard' : 'Create billboard';
   const description = initialData ? 'Edit a billboard' : 'Add a new billboard';
--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -4,26 +4,45 @@ import Heading from "@/components/heading";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
 
-const BillboardClient = () => {
+
+interface BillboardClientProps{
+  data:BillboardColumn[];
+}
+
+const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
 
   const router = useRouter()
   const params = useParams()
 
 
   return ( 
-    <div className="flex items-center justify-between">
-      <Heading
-        title='Billboards (0)'
-        description="Manage billboards for your store"
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new `)}
+        >
+          <Plus className='mr-2 h-4 w-4'/>
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable
+        columns={columns}
+        data={data}
+        searchKey="label"
       />
-      <Button
-        onClick={() => router.push(`/${params.storeId}/billboards/new `)}
-      >
-        <Plus className='mr-2 h-4 w-4'/>
-        Add new
-      </Button>
-    </div>
+
+    </>
    );
 }
  
